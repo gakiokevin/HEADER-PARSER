52984e4895e31870ff43cf9ef1b3a19df9f592e1
gakiokevin <gakiokevin5@gmail.com>
2024-02-28 19:16:00 +0300

    A header parser

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('view engine','pug')
 
 app.set('views',path.join(__dirname,'views'))
 app.get('/',(req,res)=>{
-res.render('/',{welcome:'add whoami on the url to view results'})
+res.render('home',{welcome:'add whoami on the url to view results'})
 })
 app.get('/api/whoami',(req,res)=>{
 
